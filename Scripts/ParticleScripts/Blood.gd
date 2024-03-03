6d7174f88b9ccd5184d42a797575cf6844fa1f9e
cdunham927 <cdunham927@gmail.com>
2024-03-03 15:11:16 -0800

    Scene changes

--- Scripts/ParticleScripts/Blood.gd
+++ Scripts/ParticleScripts/Blood.gd
@@ -1,9 +1,13 @@
 extends CPUParticles2D
 
 func _on_Timer_timeout():
-	#set_process(false)
-	#set_physics_process(false)
-	#set_process_input(false)
+	#get_tree().paused = true
+	
+	#print("Turn off particles")
+	set_process(false)
+	set_physics_process(false)
+	set_process_input(false)
+	#set_process(ProcessMode) = PROCESS_MODE_DISABLED
 	set_physics_process_internal(false)
-	#set_process_unhandled_input(false)
-	#set_process_unhandled_key_input(false)
+	set_process_unhandled_input(false)
+	set_process_unhandled_key_input(false)
